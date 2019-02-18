a04a7e63c5b1f07a258c9bd666bf6a25b80ef435
james.kim <james.kim@yht.co.kr>
2019-02-18 15:49:23 +0900

    commit

---
+++ src/counter.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+const Problematic = () => {
+    throw (new Error('버그가 나타났다!'));
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+class Counter extends Component {
+    state = {
+        number: 0
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({
+            error: true
+        });
+    }
+
+    handleIncrease = () => {
+        this.setState({
+            number: this.state.number + 1
+        });
+    }
+
+    handleDecrease = () => {
+        this.setState({
+            number: this.state.number - 1
+        });
+    }
+
+    render() {
+        if (this.state.error) return (<h1>에러발생!</h1>);
+
+        return (
+            <div>
+                <h1>카운터</h1>
+                <div>값: {this.state.number}</div>
+                { this.state.number === 4 && <Problematic /> }
+                <button onClick={this.handleIncrease}>+</button>
+                <button onClick={this.handleDecrease}>-</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
